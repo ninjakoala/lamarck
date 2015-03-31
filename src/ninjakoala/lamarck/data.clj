814f89110f2b76ae00688aceeb81205b52f52c1b
Neil Prosser <neil.prosser@gmail.com>
2015-03-31 13:08:21 +0100

    Update data

--- src/ninjakoala/lamarck/data.clj
+++ src/ninjakoala/lamarck/data.clj
@@ -3,24 +3,35 @@
             [clojure.java.io :as io]
             [clojure.set :as s]))
 
-(defn- numberize-prices
-  [prices]
-  (into {} (map (fn [[k v]] [k (Double/valueOf v)]) prices)))
-
-(defn- sort-out-prices
-  [pricing]
-  (into {} (map (fn [[k v]] [k (numberize-prices v)]) pricing)))
-
 (def ^:private instance-key-mappings
   {:ECU :ecu
    :ebs_optimized :ebs-optimized?
+   :enhanced_networking :enhanced-networking
    :instance_type :instance-type
+   :linux_virtualization_types :linux-virtualization-types
    :network_performance :network-performance
    :vCPU :vcpu})
+
+(def ^:private price-key-mappings
+  {:mswinSQL :mswin-sql
+   :mswinSQLWeb :mswin-sql-web})
 
 (def ^:private vpc-key-mappings
   {:ips_per_eni :ips-per-eni
    :max_enis :max-enis})
+
+(defn- double-or-nothing
+  [price]
+  (when (and (some? price) (not= price "N/A"))
+    (Double/valueOf price)))
+
+(defn- numberize-prices
+  [prices]
+  (into {} (map (fn [[k v]] (when-let [price (double-or-nothing v)] [k price])) (s/rename-keys prices price-key-mappings))))
+
+(defn- sort-out-prices
+  [pricing]
+  (into {} (map (fn [[k v]] [k (numberize-prices v)]) pricing)))
 
 (defn- adjust-instance
   [instance]
