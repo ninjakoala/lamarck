a283b9cd9559fe692627d36681a1941a73a6358f
Neil Prosser <neil.prosser@gmail.com>
2015-07-06 09:44:02 +0100

    Version 1.0.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ninjakoala/lamarck "1.0.5-SNAPSHOT"
+(defproject com.ninjakoala/lamarck "1.0.5"
   :description "For classifying clouds (specifically Amazon's)"
   :url "https://github.com/ninjakoala/lamarck"
   :license {:name "Eclipse Public License"
