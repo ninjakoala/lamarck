d2fe0227c0d2fd14688368617110a2ca2917e5a5
Neil Prosser <neil.prosser@gmail.com>
2016-05-20 22:21:13 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/ninjakoala/lamarck"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.5.0"]
-                 [com.taoensso/encore "1.38.0"]
+  :dependencies [[cheshire "5.6.1"]
+                 [com.taoensso/encore "2.53.0"]
                  [org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}}
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2"]]}}
   :plugins [[lein-release "1.0.5"]]
   :lein-release {:deploy-via :clojars})
