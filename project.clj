9d05d76bee757648e3e454dd59fd930f739b3c04
Neil Prosser <neil.prosser@gmail.com>
2017-06-16 21:09:05 +0100

    Update project configuration

--- project.clj
+++ project.clj
@@ -3,10 +3,17 @@
   :url "https://github.com/ninjakoala/lamarck"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.6.3"]
-                 [com.taoensso/encore "2.87.0"]
+  :dependencies [[cheshire "5.7.1"]
+                 [com.taoensso/encore "2.91.1"]
                  [org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
                    :plugins [[lein-midje "3.2.1"]]}}
-  :plugins [[lein-release "1.0.5"]]
-  :lein-release {:deploy-via :clojars})
+  :aliases {"test" ["midje"]}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
--- update-data.sh
+++ update-data.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 curl --silent "https://raw.githubusercontent.com/powdahound/ec2instances.info/master/www/instances.json" | python -m json.tool > resources/instances.json
